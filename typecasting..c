ae0b6117faa94b6f600772ba5eaadf96252642e5
bannysha <manyapubannysha2000@gmail.com>
2022-04-05 21:03:35 +0530

    pythonbasics

---
+++ typecasting..c
@@ -0,0 +1,29 @@
+a=10
+b=0.5
+c=0
+type(c)
+o/p=<class 'int'>
+c=a+b
+type(c)
+o/p=<class 'float'>
+a=10
+float(a)
+o/p=10.0
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
+o/p=1
+2
+3
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
+o/p=s1
+2
+3.0
