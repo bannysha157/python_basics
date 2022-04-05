93e614bcda00ce7bdfab7b0b4c9f9cf039257181
bannysha <manyapubannysha2000@gmail.com>
2022-04-05 20:14:34 +0530

    pythonbasics

---
+++ membershipoperator.c
@@ -0,0 +1,21 @@
+
+
+x=["apple","banana"]
+print("banana" in x) #returns True because a sequence with the value "banana" is in the list
+
+o/p=True
+
+
+str1="python"
+b1="o"in str1
+b2='e' in str1
+print(b1)
+o/p=True
+print(b2)
+o/p=False
+b3="e" not in str1
+print(b3)
+o/p=True
+
+
+
