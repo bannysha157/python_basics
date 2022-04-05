f44d8a18aaebcd5861d8631062f1ad75303eb511
bannysha <manyapubannysha2000@gmail.com>
2022-04-05 19:56:46 +0530

    pythonbasics

---
+++ assignmentoperator.c
@@ -0,0 +1,53 @@
+
+x=5
+print(x)
+o/p=5
+x=5
+x += 3
+print(x)
+o/p=8
+x=7
+x -=3
+print(x)
+o/p=4
+x=5
+x*=5
+print(x)
+o/p=25
+x=6
+x /=4
+print(x)
+o/p=1.5
+x=5
+x%=7
+print(x)
+o/p=5
+x=5
+x **=5
+print(x)
+o/p=3125
+x=3
+x**=3  #3*3=9,9*3=27
+print(x)
+o/p=27
+a=3
+a&=4
+print(x)
+o/p=27
+
+a=5
+a|= 3
+print(a)
+o/p=7
+a=5
+a^=5
+print(a)
+o/p=0
+x=5
+x>>=3
+print(x)
+0
+a=5
+a<<=3
+print(a)
+o/p=40
