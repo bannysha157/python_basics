8337f83ea2fced42b4a5fdae2bd50d8cd3d2ce43
bannysha <manyapubannysha2000@gmail.com>
2022-04-05 22:50:07 +0530

    pythonbasics

---
+++ rangefunctions.c
@@ -0,0 +1,51 @@
+for index in range(1,20,2):
+  print(index) #2 indicates two steps forward
+
+#Output
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+
+
+for index in range(20,1,-2):
+  print(index)
+
+#Output
+20
+18
+16
+14
+12
+10
+8
+6
+4
+2
+
+for index in range(-5,0):
+  print(index)
+
+#Output
+-5
+-4
+-3
+-2
+-1
+
+x = range(6)
+
+for n in x:
+  print(n)
+o/p=0
+1
+2
+3
+4
+5
