641a573edf74f1620912ebd59a25fd459cc76389
bannysha <manyapubannysha2000@gmail.com>
2022-04-05 22:59:31 +0530

    pythonbasics

---
+++ bitwiseoperator.c
@@ -0,0 +1,37 @@
+bitwise and(&)
+a = 7
+b = 4
+c = 5
+print(a & b)
+print(a & c)
+print(b & c)
+
+o/p=4
+5
+4
+bitwise or(|)
+
+a = 7
+b = 4
+c = 5
+print(a | b)
+print(a | c)
+print(b | c)
+o/p=7
+7
+5
+bit wise xor(^)
+
+a = 7
+b = 4
+c = 5
+print(a ^ c)
+print(b ^ c)
+o/p=3
+2
+1
+
+
+
+
+
