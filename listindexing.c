c1e280a11ef314e8e26095a5674524a2ab1c5411
bannysha <manyapubannysha2000@gmail.com>
2022-04-06 19:08:21 +0530

    pythonbasics

---
+++ listindexing.c
@@ -0,0 +1,47 @@
+s="bannysha"
+print("s[0]: ",s[0])
+s[0]:  b
+print("s[1]: ",s[1])
+s[1]:  a
+print("s[2]: ",s[2])
+s[2]:  n
+print("s[3]: ",s[3])
+      
+s[3]:  n
+print("s[4]: ",s[4])
+      
+s[4]:  y
+print("s[5]: ",s[5])
+      
+s[5]:  s
+print("s[6]: ",s[6])
+      
+s[6]:  h
+print("s[7]: ",s[7])
+      
+s[7]:  a
+print("s[-1]: ",s[-1])
+      
+s[-1]:  a
+print("s[-2]: ",s[-2])
+      
+s[-2]:  h
+print("s[-3]: ",s[-3])
+      
+s[-3]:  s
+print("s[-4]: ",s[-4])
+      
+s[-4]:  y
+print("s[-5]: ",s[-5])
+      
+s[-5]:  n
+print("s[-6]: ",s[-6])
+      
+s[-6]:  n
+print("s[-7]: ",s[-7])
+      
+s[-7]:  a
+print("s[-8]: ",s[-8])
+      
+s[-8]:  b
+
